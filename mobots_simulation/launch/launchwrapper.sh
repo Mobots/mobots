294d67141d19ea7dda5ee55bd646629071c512c0
David <david@informatik.uni-bremen.de>
2011-12-20 23:48:21 +0100

    Dateien reorganisiert und Straßenkarte eingebunden.

--- mobots_simulation/launch/launchwrapper.sh
+++ mobots_simulation/launch/launchwrapper.sh
@@ -6,8 +6,8 @@ sleep 2
 rosrun image_view image_view image:=/my_cam/image &
 xterm -e rosrun mobots_simulation keyboard & #_walk_vel:=0.1 _run_vel:=0.5 &
 rosrun mobots_simulation mobots_teleop_bridge _walk_vel:=0.01 _run_vel:=0.5 &
-rosparam load $(rospack find mobots_simulation)/models/gazebo_camera.urdf test_cam
-roslaunch mobots_simulation streetmap.launch &
+rosparam load $(rospack find mobots_simulation)/models/mobot.urdf test_cam
+roslaunch mobots_simulation mobots_world.launch &
 gazebo_pid=$(echo $!)
 sleep 2
 rosrun gazebo spawn_model -param test_cam -urdf -model mobot -z 3 #-P 75 -Y 90 -R 50
