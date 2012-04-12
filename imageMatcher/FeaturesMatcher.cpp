d70bc788dfc080dc3a091a7bec82ad51f92776b3
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-04-13 01:52:44 +0200

    stitching now partially working

--- imageMatcher/FeaturesMatcher.cpp
+++ imageMatcher/FeaturesMatcher.cpp
@@ -21,11 +21,15 @@ CpuFeaturesMatcher::CpuFeaturesMatcher(const string& type){
   matcher = DescriptorMatcher::create(type);
 }
 
-double toDegree(double rad){
+inline double avg(double d1, double d2){
+  return (d1+d2)/2;
+}
+
+inline double toDegree(double rad){
   return rad * 180 / 3.14159265;
 }
 
-void CpuFeaturesMatcher::findDelta(const ImageFeatures& img1, const ImageFeatures& img2, Delta& delta) const{
+void CpuFeaturesMatcher::match(const ImageFeatures& img1, const ImageFeatures& img2, ImageMatchResult& result) const{
   std::vector<DMatch> matches;
   std::vector<DMatch> good_matches;
     
@@ -39,7 +43,6 @@ void CpuFeaturesMatcher::findDelta(const ImageFeatures& img1, const ImageFeature
   for(int i = 0; i < N; i++){ 
     if(matches[i].distance < 2*min_dist)
      good_matches.push_back(matches[i]); 
-    //if(good_matches.size() > 2) break;
   }
   cout << "match count " << good_matches.size() << endl;  
   std::vector<Point2f> points1;
@@ -48,16 +51,22 @@ void CpuFeaturesMatcher::findDelta(const ImageFeatures& img1, const ImageFeature
     points1.push_back(img1.keypoints[good_matches[i].queryIdx].pt);
     points2.push_back(img2.keypoints[good_matches[i].trainIdx].pt);
   }
-  //Mat affine = getAffineTransform(points1, points2);
-  //cout << "affine " << affine << endl;
-  Mat transformMatrix = findHomography(points1, points2, CV_RANSAC, 3);
-  delta.x = transformMatrix.at<double>(0, 2);
-  delta.y = transformMatrix.at<double>(1, 2);
-  delta.theta = min(toDegree(sin(transformMatrix.at<double>(0,1))), toDegree(cos(transformMatrix.at<double>(0,0))));
+  Mat transformMatrix = findHomography(points1, points2, CV_RANSAC);
+  /* Matrix form:
+   * cos(theta)  -sin(theta) deltaX
+   * sin(theta)   cos(theta) deltaY
+   *    0             0        1
+   */
+  result.delta.x = transformMatrix.at<double>(0, 2);
+  result.delta.y = transformMatrix.at<double>(1, 2);
+  double majorDiag = acos(avg( transformMatrix.at<double>(0,0), transformMatrix.at<double>(1,1)));
+  double minorDiag = asin(avg(-transformMatrix.at<double>(0,1), transformMatrix.at<double>(1,0)));
+  result.delta.theta = avg(majorDiag, minorDiag);
+  result.H = transformMatrix;
   
-  std::cout << "Mat " << transformMatrix << std::endl;
-  cout << toDegree(cos(transformMatrix.at<double>(0,0)))  << "° " << toDegree(sin(transformMatrix.at<double>(0,1))) << "°" << endl;
-  cout << toDegree(-sin(transformMatrix.at<double>(1,0))) << "° " << toDegree(cos(transformMatrix.at<double>(1,1))) << "°" << endl;
+  std::cout << "rot Mat" << endl;
+  cout << toDegree(acos(transformMatrix.at<double>(0,0)))  << "° " << toDegree(asin(-transformMatrix.at<double>(0,1))) << "°" << endl;
+  cout << toDegree(asin(transformMatrix.at<double>(1,0))) << "° " << toDegree(acos(transformMatrix.at<double>(1,1))) << "°" << endl;
   
     //-- DEBUG Show detected matches
   Mat img_matches;
