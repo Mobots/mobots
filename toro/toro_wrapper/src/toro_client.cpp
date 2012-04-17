9dc7f4a930ff3676a59c2ec16572376e7bc488b8
Moritz Ulmer <moritz89@gmail.com>
2012-04-17 14:29:47 +0200

    Image_transport works. Next step is to integrate it with ImageWithPose.msg Added: 'Depends' to Manifest.xml - opencv and co.

--- toro/toro_wrapper/src/toro_client.cpp
+++ toro/toro_wrapper/src/toro_client.cpp
@@ -1,13 +1,32 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "mobots_msgs/MobotImagePose.h"
+#include "image_transport/image_transport.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+#include "cv_bridge/CvBridge.h"
+#include "iostream"
+#include "fstream"
 
 /**
  * The imageHandler method.
  */
-void imageHandler(const mobots_msgs::MobotImagePose::ConstPtr& msg)
+void imageHandler(const mobots_msgs::ImageWithPose::ConstPtr& msg)
 {
--	ROS_INFO("MobotID: %i / Header: %i - %s / image: %s", msg->mobotID, msg->header.seq, msg->header.frame_id, msg->imagePose.image.encoding);
+	ROS_INFO("MobotID: %i / Header: %i - %s / image: %s", msg->mobotID, msg->image.header.seq, msg->image.header.frame_id.c_str(), msg->image.encoding.c_str());
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+	ROS_INFO("Got a callback!");
+  sensor_msgs::CvBridge bridge;
+  try
+  {
+    cvShowImage("view", bridge.imgMsgToCv(msg, "bgr8"));
+  }
+  catch (sensor_msgs::CvBridgeException& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
 }
 
 /**
@@ -19,8 +38,16 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "toro_client");
 	ros::NodeHandle handle;
 	// The topic name is mobot_image_pose
-	ros::Subscriber sub = handle.subscribe("mobot_image_pose", 1000, imageHandler);
+	/*ros::Subscriber sub = handle.subscribe("mobot_image_pose", 1000, imageHandler);
 	ROS_INFO("Check: Spin-pre");
 	ros::spin();
+	return 0;*/
+	
+	cvNamedWindow("view");
+	cvStartWindowThread();
+	image_transport::ImageTransport it(handle);
+	image_transport::Subscriber sub = it.subscribe("mobot_image_pose", 1, imageCallback);
+	ros::spin();
+	cvDestroyWindow("view");
 	return 0;
 }
--- toro/toro_wrapper/src/toro_server_test.cpp
+++ toro/toro_wrapper/src/toro_server_test.cpp
@@ -1,6 +1,12 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "mobots_msgs/MobotImagePose.h"
+#include "image_transport/image_transport.h"
+#include "opencv/cvwimage.h"
+#include "opencv/highgui.h"
+#include "cv_bridge/CvBridge.h"
+#include "iostream"
+#include "fstream"
 
 /**
  * This server is used to check if the toro_client.cpp works.
@@ -11,23 +17,21 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "toro_server_test");
 	ros::NodeHandle handle;
-	ros::Publisher pub = handle.advertise<mobots_msgs::MobotImagePose>("mobot_image_pose", 1000);
+	image_transport::ImageTransport it(handle);
+	image_transport::Publisher pub = it.advertise("mobot_image_pose", 1);
+	cv::WImageBuffer3_b image( cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR) );
+	sensor_msgs::ImagePtr msg = sensor_msgs::CvBridge::cvToImgMsg(image.Ipl(), "bgr8");
+	
 	ros::Rate loop_rate(1);
 	
 	int count = 0;
-	while (ros::ok())
+	while (handle.ok())
 	{
-		mobots_msgs::MobotImagePose msg;
-		msg.mobotID = count;
-		
-		ROS_INFO("MobotID: %i", msg.mobotID);
-		
+		ROS_INFO("Sent msg no: %i", count);
 		pub.publish(msg);
-		
 		ros::spinOnce();
-		
 		loop_rate.sleep();
-		++count;
+		count++;
 	}
 	
 	return 0;
