b8e41b196da0d64d49ffdad51f463f5415d468b2
Moritz Ulmer <moritz89@gmail.com>
2012-04-20 15:53:46 +0200

    Changed to CompressedImage. Working data transfer.

--- toro/toro_wrapper/src/toro_server_test.cpp
+++ toro/toro_wrapper/src/toro_server_test.cpp
@@ -7,6 +7,7 @@
 #include "cv_bridge/CvBridge.h"
 #include "iostream"
 #include "fstream"
+#include "vector"
 
 /**
  * This server is used to check if the toro_client.cpp works.
@@ -17,23 +18,46 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "toro_server_test");
 	ros::NodeHandle handle;
-	image_transport::ImageTransport it(handle);
-	image_transport::Publisher pub = it.advertise("mobot_image_pose", 1);
-	cv::WImageBuffer3_b image( cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR) );
-	sensor_msgs::ImagePtr msg = sensor_msgs::CvBridge::cvToImgMsg(image.Ipl(), "bgr8");
+	ros::Publisher pub = handle.advertise<mobots_msgs::ImageWithPose>("mobot_image_pose", 10);
+	
+	mobots_msgs::ImageWithPose msg;
+	msg.pose.pose.pose.position.x = 0;
+	msg.pose.pose.pose.position.y = 42;
+	msg.pose.pose.pose.position.z = 100;
+	msg.image.format = "jpeg";
+	std::ifstream imageFile(argv[1], std::ios::binary);
+	imageFile.seekg(0, std::ios::end);
+	int length = imageFile.tellg();
+	ROS_INFO("length: %i", length);
+	char buffer[length];
+	imageFile.seekg(0, std::ios::beg);
+	
+	imageFile.read(buffer, length);
+	ROS_INFO("readcheck. [0]=%i, [1]=%i",buffer[0], buffer[1]);
+	msg.image.data.assign(buffer, buffer + sizeof(buffer)/sizeof(char));
+	ROS_INFO("data.size: %i", msg.image.data.size());
+	
+	/*msg.image.data.assign(length, 0);
+	imageFile.seekg(0, std::ios::beg);
+	imageFile.read(reinterpret_cast<char*>(&msg.image.data[0]), length);*/
 	
 	ros::Rate loop_rate(1);
 	
 	int count = 0;
 	while (handle.ok())
 	{
+		
 		ROS_INFO("Sent msg no: %i", count);
 		pub.publish(msg);
 		ros::spinOnce();
 		loop_rate.sleep();
-		count++;
+		
+/*		
+		*/
+		msg.pose.pose.pose.position.x++;
+		msg.pose.pose.pose.position.y++;
+		msg.pose.pose.pose.position.z++;
 	}
 	
 	return 0;
 }
-	
