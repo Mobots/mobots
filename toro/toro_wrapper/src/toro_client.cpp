761fccdfcc3edd71a6f54a8fa1b34fa1d3b76b7c
Moritz Ulmer <moritz89@gmail.com>
2012-04-24 15:04:18 +0200

    added a configHandler

--- toro/toro_wrapper/src/toro_client.cpp
+++ toro/toro_wrapper/src/toro_client.cpp
@@ -4,11 +4,12 @@
 #include "fstream"
 
 int mobotNumber = 3; //Anzahl der Mobots
-int *imageCounter;
-std::ofstream imageFile;
-std::ofstream toroFile;
 std::string savePath = ""; //used to change the default save path
-const std::string naming = "%sid%i-%i.%s"; // /home/john/id1-12.jpeg
+const std::string naming = "%s%i/id%i-%i.%s"; // /home/john/1/id1-12.jpeg
+
+int *imageCounter;
+int session = 0;
+std::ofstream toroFile;
 
 /**
  * The imageHandler method. 
@@ -16,14 +17,13 @@ const std::string naming = "%sid%i-%i.%s"; // /home/john/id1-12.jpeg
  * TODO add session IDs and folders
  * TODO add jpeg checker. Save as "*.jpg"
  */
-void imageHandler(const mobots_msgs::ImageWithPose::ConstPtr& msg)
-{
+void imageHandler(const mobots_msgs::ImageWithPose::ConstPtr& msg){
 	ROS_INFO("MobotID: %i / Pose: %f,%f,%f", msg->mobotID, msg->pose.pose.pose.position.x, msg->pose.pose.pose.position.y, msg->pose.pose.pose.position.z);
 	char imagePath[1000];
 	sprintf(imagePath, naming.c_str(), savePath.c_str(), msg->mobotID, imageCounter[msg->mobotID], msg->image.format.c_str());
 	ROS_INFO("%s", imagePath);
 	
-	imageFile.open(imagePath, std::ios::binary);
+	std::ofstream imageFile(imagePath, std::ios::binary);
 	ROS_INFO("data.size: %i", msg->image.data.size());
 	imageFile.write((const char*) &(msg->image.data[0]), (std::streamsize) msg->image.data.size());
 	imageFile.close();
@@ -32,19 +32,41 @@ void imageHandler(const mobots_msgs::ImageWithPose::ConstPtr& msg)
 }
 
 /**
+ * The configHandler method. An API to set variables.
+ * TODO getter interface
+ * TODO setter for session and imageCounter
+ */
+void configHandler(const mobots_msgs::Config::ConstPtr& msg){
+	string key;
+	if(msg->node.compare("toro_client")){
+		if(msg->key.compare("savePath")){
+			ROS_INFO("Change option: savePath from %s to %s", savePath, msg->value);
+			savePath = msg->value;
+		} else if(msg->key.compare("mobotNumber")){
+			ROS_INFO("Change option: mobotNumber from %s to %s", mobotNumber, msg->value);
+			mobotNumber = atoi(msg->value);
+		} else if(msg->key.compare("naming")){
+			ROS_INFO("Change option: naming from %s to %s", naming, msg->value);
+			naming = msg->value;
+		} else {
+			ROS_INFO("No matching options");
+		}
+	}
+}
+
+/**
  * The main method.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	// The node is called toro_client
 	ros::init(argc, argv, "toro_client");
 	ros::NodeHandle handle;
 	// The topic name is mobot_image_pose
 	ros::Subscriber sub = handle.subscribe("mobot_image_pose", 10, imageHandler);
+	ros::Subscriber subConfig = handle.subscribe("mobot_config", 1000, configHandler);
 	
 	imageCounter = new int[mobotNumber];
-	for(int i = 0; i < mobotNumber; i++)
-	{
+	for(int i = 0; i < mobotNumber; i++){
 		imageCounter[i] = 0;
 	}
 	
