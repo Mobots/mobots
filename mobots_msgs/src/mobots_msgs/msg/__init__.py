93ef38b0cbc39021c83d2f958770066222d9f2d0
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-04-26 22:41:26 +0200

    deleted config package, added messages for featureDetector node

---
+++ mobots_msgs/src/mobots_msgs/msg/__init__.py
@@ -0,0 +1,7 @@
+from ._FeatureDescriptor import *
+from ._CvMat import *
+from ._ImageFeatures import *
+from ._FeaturePoint import *
+from ._Point2f import *
+from ._Config import *
+from ._ImageWithPose import *
