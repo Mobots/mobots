b7b3e96e5bdf0319ffc167484a1fb6132c3a4891
Hauke Hansen <hauke.hansen@uni-bremen.de>
2012-05-07 10:52:16 +0200

    shutter prealpha

---
+++ shutter/src/shutter.h
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <../../opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp/include/ros/ros.h>
+#include <../../home/hugebird/ros_workspace/shutter/msg_gen/cpp/include/shutter/ImagePoseID.h>
+#include <math.h>
+
+class Shutter {
+
+
+public:
+    Shutter(int mobot_ID);
+    ~Shutter();
+    void imageCallback(const sensor_msgs::Image& mobot_image);
+    void mouseCallback(const geometry_msgs::Pose2D& mouse_data);
+    void startShutter();
+    void publishMessage(double& x, double& y, double& theta, sensor_msgs::Image& image);
+    double checkPicture(double x, double y, double theta);
+
+    ros::Subscriber pose_sub;
+    ros::Subscriber image_sub;
+    ros::Publisher poseImage_pub;
+    shutter::ImagePoseID ipid;
+    int id;
+    double const overlap = 0.2;
+    double dX, dY, dTheta, globalX, globalY, globalTheta;
+    int argv;
+    char argc;
+    ros::NodeHandle nh;
+
+};
