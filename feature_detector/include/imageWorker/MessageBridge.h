d7061badb93eefba76672df692350fae47524e24
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-05-08 02:28:01 +0200

    updated mobox mouse node work with 2 mice

--- feature_detector/include/imageWorker/MessageBridge.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace MessageBridge{
-  void copyToRosMessage(const ImageFeatures& in, mobots_msgs::FeatureSetWithDeltaPose& out);
-  void copyToCvStruct(const mobots_msgs::FeatureSetWithDeltaPose& in, ImageFeatures& out);
-}
--- mobox/src/mouse.cpp
+++ mobox/src/mouse.cpp
@@ -1,39 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
 #include <ros/ros.h>
-/*#include <gazebo_msgs/ModelState.h>
-#include <gazebo_msgs/SetModelState.h>
-#include <gazebo_msgs/GetModelState.h>
+#include <iostream>
+#include <fcntl.h>
+#include <pthread.h>
+#include <cmath>
 
-ros::ServiceClient gazeboSetClient;
-ros::ServiceClient gazeboGetClient;*/
+#include "geometry_msgs/Pose2D.h"
 
-void handleSignal(int signal){
-  exit(0);
+using namespace std;
+
+char xAvg;
+char yAvg;
+float theta;
+
+void* publisherThread(void* data){
+  ros::Rate rate(50);
+  ros::NodeHandle nodeHandle;
+  ros::Publisher pub = nodeHandle.advertise<geometry_msgs::Pose2D>("/mouse/pose", 2);
+  geometry_msgs::Pose2D deltaPose;
+  while(ros::ok()){
+    deltaPose.x = xAvg;
+    deltaPose.y = yAvg;
+    deltaPose.theta = theta;
+    pub.publish(deltaPose);
+    rate.sleep();
+  }
 }
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "mouse_node");
-  
-  /*ros::NodeHandle n;
-  gazeboSetClient = n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
-  gazeboSetClient.waitForExistence();
-  gazeboGetClient = n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state", true);
-  gazeboGetClient.waitForExistence();*/
-  
-  FILE *fmouse;
-  char b[3];
-  fmouse = fopen("/dev/input/mice", "r");
-  int xd=0,yd=0; //x/y movement delta
+  int mouse1 = open("/dev/input/mouse0", O_RDONLY);
+  int mouse2 = open("/dev/input/mouse1", O_RDONLY);
+  if(mouse1 < 0){
+    cout << "cannot open mouse 1" << endl;
+    exit(1);
+  }else if(mouse2 < 0){
+    cout << "cannot open mouse 2" << endl;
+    exit(1);
+  }
+  pthread_t thread;
+  pthread_create(&thread, 0, publisherThread, 0);
+  /*int xd=0,yd=0; //x/y movement delta
   int xo=0,yo=0; //x/y overflow (out of range -255 to +255)
-  int lb=0,mb=0,rb=0,hs=0,vs=0; //left/middle/right mousebutton
-
-  signal(SIGINT, handleSignal);
-  while(1){
-    fread(b, sizeof(char), 3, fmouse);
-    lb=(b[0]&1)>0;
+  int lb=0,mb=0,rb=0,hs=0,vs=0; //left/middle/right mousebutton*/
+  
+  char data1[3];
+  char data2[3];
+  ros::Rate rate(50);
+  while(ros::ok()){
+    read(mouse1, &data1, 3);
+    read(mouse2, &data2, 3);
+    xAvg = (data1[1]/2) + (data2[1]/2) + ((data1[1]&0x1) & (data2[1]&0x1));
+    yAvg = (data1[2]/2) + (data2[2]/2) + ((data1[2]&0x1) & (data2[2]&0x1));
+    //das folgende muss noch durch nen induktionsbeweis verifiziert werden :D
+    theta = atan2(data2[2]-data1[2], data2[1]-data1[1]);
+    rate.sleep();
+    /*lb=(b[0]&1)>0;
     rb=(b[0]&2)>0;
     mb=(b[0]&4)>0;
     hs=(b[0]&16)>0;
@@ -42,28 +64,6 @@ int main(int argc, char **argv){
     yo=(b[0]&128)>0;
     xd=b[1];
     yd=b[2];
-    printf("xdelta = %d ydelta = %d\n", xd, yd);
-    /*gazebo_msgs::GetModelState getState;
-    getState.request.model_name = std::string("mobot");
-    getState.request.relative_entity_name = std::string("world");
-    gazeboGetClient.call(getState);
-
-    gazebo_msgs::ModelState modelState;
-    modelState.model_name = std::string("mobot");
-    modelState.reference_frame = std::string("world"); 
-
-    modelState.twist.linear.x = double(xd); //b[1];
-    modelState.twist.linear.y = double(yd); //b[2];
-    modelState.twist.linear.z = 0;
-    modelState.twist.angular.x = 0;
-    modelState.twist.angular.y = 0;
-    modelState.twist.angular.z = 0;
-    
-    modelState.pose = getState.response.pose;
-    
-    gazebo_msgs::SetModelState setModelState; 
-    setModelState.request.model_state = modelState;
-    gazeboSetClient.call(setModelState);*/
+    printf("xdelta = %d ydelta = %d\n", xd, yd);*/
   }
-  fclose(fmouse);
 }
