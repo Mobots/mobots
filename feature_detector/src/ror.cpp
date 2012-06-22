2c9a785c8f6107a98aadc0985807efb4f376d876
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-06-23 00:00:39 +0200

    current status

--- feature_detector/src/ror.cpp
+++ feature_detector/src/ror.cpp
@@ -33,28 +33,8 @@ float euclideanDistance(const Point2f& p1, const Point2f& p2){
   return sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
 }
 
-//stackoverflow
-unsigned int nCr(unsigned int n, unsigned int k){
-    if (k > n) return 0;
-    if (k * 2 > n) k = n-k;
-    if (k == 0) return 1;
-    unsigned int result = n;
-    for(unsigned int i = 2; i <= k; ++i) {
-        result *= (n-i+1);
-        result /= i;
-    }
-    return result;
-}
-
 inline float getHeading(const Point2f& p1, const Point2f& p2){
   return atan2(p2.y-p1.y, p2.x-p1.x);
-}
-
-inline float getRotation(const Point2f* p1, const Point2f* p2){
-  float heading1 = getHeading(p1[0], p1[1]);
-  float heading2 = getHeading(p2[0], p2[1]);
-  float diff = heading2-heading1;
-  return diff;
 }
 
 inline float getRotation(const Point2f& p11, const Point2f& p12, const Point2f& p21, const Point2f& p22){
@@ -80,45 +60,23 @@ inline void rotate(const Point2f& in, Point2f& out, float angle){
  * complexity is at least nC2
  */
 bool rorAlternative(const vector<Point2f>& points1, const vector<Point2f>& points2, Delta& delta){
+  //additional ideas:
   //first simple matching: p1[i],p1[i+1] <--> p2[i],p2[i+1]
   // 		     and: p1[i],p1[i+2] <--> p2[i],p2[i+2]
   //if one rotation get's more than 60% it's ok.
-  /*int counts[] = new int[2*M_PI/threshold +1]; //2° steps
-  float sums[] = new float[2*M_PI/threshold +1];
-  int count = 0;
-  for(int i = 0; i < points1.size(); i+=2){
-    float rot = calcRotation(&points1[i], &points2[i]);
-    int index = rot/threshold;
-    counts[index]++;
-    sums[index] += rot;
-    count++;
-  }
-  for(int i = 0; i < 2*M_PI/threshold +1; i++){
-    sums[i] /= counts[i];
-    cout << i*threshold << " = " << toDegree(i*threshold) << " => " << counts[i] << " times" << endl;
-  }*/
   
-   unsigned int counts[N];
+ unsigned int counts[N] = {0};
  float sumsTheta[N] = {0};
  float sumsX[N] = {0};
  float sumsY[N] = {0};
-   unsigned int* const countsMid = &counts[N/2];
+ unsigned int* const countsMid = &counts[N/2];
  float* const sumsMid = &sumsTheta[N/2];
  float* const sumsMidX = &sumsX[N/2];
  float* const sumsMidY = &sumsY[N/2];
-  memset(&counts, 0, N*sizeof(int));
-  //memset(&sumsTheta, 0.0, N*sizeof(float));
-  //memset(&sumsX, 0, N*sizeof(float));
-  //memset(&sumsY, 0, N*sizeof(float));
   const int p1Size = points1.size();
-  vector<int> indices(p1Size);
-  for(int i = 0; i < p1Size; i++){
-    indices[i] = i;
-  }
   for(int i1 = 0; i1 < p1Size; i1++){
-    for(int i2 = i1; i2 < p1Size; i2++){
-      float rot = getRotation(points1[i1], points1[i2], points2[i1], points2[i2]);
-      cout << rot << "=" << toDegree(rot) << "°" << endl;
+    for(int i2 = i1+1; i2 < p1Size; i2++){
+      float rot = getRotation(points1.at(i1), points1.at(i2), points2.at(i1), points2.at(i2));
       float origR = rot;
       if(rot > M_PI)
 	rot = 2*M_PI - rot;
@@ -127,22 +85,11 @@ bool rorAlternative(const vector<Point2f>& points1, const vector<Point2f>& point
       if(abs(
 	euclideanDistance(points1[i1], points1[i2]) 
 	- euclideanDistance(points2[i1], points2[i2])
-	    ) < 3){
-	    /*cout << i << "+" << i2 << " " << points1[i].x << "x " << points1[i].y << "y <->" << points1[i2].x << "x " << points1[i2].y << "y" 
-	    << " :: dist " << euclideanDistance(points1[i], points1[i2]) << endl 
-	    
-	    << i << "+" << i2 << " "  << points2[i].x << "x " << points2[i].y << "y <->" << points2[i2].x << "x " << points2[i2].y << "y" 
-	    << " :: dist " << euclideanDistance(points1[i], points1[i2]) << endl
-	    << " ||| " << abs(euclideanDistance(points1[i], points1[i2]) - euclideanDistance(points1[i], points1[i2])) << endl; 
-	/*cout << euclideanDistance(points1[indices[0]], points2[indices[0]]) << "  "
-      << euclideanDistance(points1[indices[0]], points1[indices[1]]) << "  "
-      << euclideanDistance(points2[indices[0]], points2[indices[1]]) << "  "
-      << indices[0] << "|" << indices[1] << "-> " << rot << " = " << toDegree(rot) << "°" << endl;*/
-      }else{
-	cout << "sorted out " << i1 << "-" << i2 << endl;
+	    ) > 3){
+	//cout << "sorted out " << i1 << "-" << i2 << endl;
 	continue;
       }
-      int index = rot/rotationStep;
+      int index = round(rot/rotationStep);
       countsMid[index]++;
       sumsMid[index] += rot;
       Point2f b;
@@ -151,30 +98,6 @@ bool rorAlternative(const vector<Point2f>& points1, const vector<Point2f>& point
       sumsMidY[index] += points1.at(i1).y - b.y;
     }
   }
-  /*while(boost::next_combination(indices.begin(), indices.begin()+2, indices.end())){     
-    float rot = getRotation(&points1[indices[0]], &points2[indices[0]]);
-    float origR = rot;
-    if(rot > M_PI)
-      rot = 2*M_PI - rot;
-    else if(rot < -M_PI)
-      rot = -2*M_PI - rot;
-    int i = indices[0];
-    int i2 = indices[1];
-    if(abs(
-      euclideanDistance(points1.at(indices[0]), points1.at(indices[1])) 
-      - euclideanDistance(points2.at(indices[0]), points2.at(indices[1]))
-	  ) > 3){
-      cout << "sorted out " << i << "-" << i2 << endl;
-      continue;
-    }
-    int index = rot/rotationStep;
-    countsMid[index]++;
-    sumsMid[index] += rot;
-    Point2f b;
-    rotate(points2[indices[0]], b, -origR);
-    sumsMidX[index] += points1.at(indices[0]).x - b.x;
-    sumsMidY[index] += points1.at(indices[0]).y - b.y;
-  }*/
   float bestAvg;
   int bestAvgCount = 0;
   int bestIndex;
