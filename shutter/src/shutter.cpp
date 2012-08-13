ea6405666a03197595636fe02645ce9bd642d73f
flo <flok912@yahoo.de>
2012-08-13 15:34:16 +0200

    shutter update

--- shutter/src/shutter.cpp
+++ shutter/src/shutter.cpp
@@ -20,26 +20,32 @@ Shutter::~Shutter() {
 void Shutter::startShutter()
 {
     ROS_INFO("Mobot %d: Shutterfunktion gestartet.", id);
-    poseImage_pub = nh.advertise<mobots_msgs::ImagePoseID>("/mobot_pose/ImagePoseID", 2);
+    poseImage_pub = nh.advertise<mobots_msgs::ImageWithDeltaPoseAndID>("/mobot_pose/ImageWithDeltaPoseAndID", 2);
 
     image_sub = nh.subscribe("/my_cam/image", 5, &Shutter::imageCallback, this);
     pose_sub = nh.subscribe("/mouse/pose", 100, &Shutter::mouseCallback, this);
 
+    ros::ServiceServer service = nh.advertiseService("getDelta", &Shutter::getDelta, this);
     
-    overlap = 0.3;
+    ros::param::param<double>("overlap",overlap, 0.3);
     dX = 0;
     dY = 0;
     dTheta = 0;
-    globalTheta = 0;
-    globalX = 0;
-    globalY = 0;
 
     ros::spin();
 
 }
 
+bool Shutter::getDelta(shutter::delta::Request &req, shutter::delta::Response &res)
+{
+  res.x = dX;
+  res.y = dY;
+  res.theta = dTheta;
+  return true;
+}
+
 void Shutter::publishMessage(double &x, double &y, double &theta, const sensor_msgs::Image &image) {
-    ipid.Mobot_ID = id;
+    ipid.mobot_id = id;
     ipid.image = image;
 
     geometry_msgs::Pose2D pose;
@@ -49,7 +55,6 @@ void Shutter::publishMessage(double &x, double &y, double &theta, const sensor_m
 
     ipid.pose = pose;
     poseImage_pub.publish(ipid);
-
 }
 
 
@@ -66,11 +71,9 @@ void Shutter::imageCallback(const sensor_msgs::Image &mobot_image) {
 
 void Shutter::mouseCallback(const geometry_msgs::Pose2D &mouse_data) {
     dX += mouse_data.x;
-    globalX += mouse_data.x;
     dY += mouse_data.y;
-    globalY += mouse_data.y;
     dTheta += mouse_data.theta;
-    globalTheta += mouse_data.theta;
-
-
 }
+
+
+
--- shutter/src/shutter.h
+++ shutter/src/shutter.h
@@ -1,7 +1,8 @@
 #include <cstdio>
 #include <iostream>
 #include <ros/ros.h>
-#include <mobots_msgs/ImagePoseID.h>
+#include "shutter/delta.h"
+#include <mobots_msgs/ImageWithDeltaPoseAndID.h>
 #include <math.h>
 #include "geometry.h"
 
@@ -16,22 +17,24 @@ public:
     void imageCallback(const sensor_msgs::Image &mobot_image);
     void mouseCallback(const geometry_msgs::Pose2D &mouse_data);
     void startShutter();
-    void publishMessage(double& x, double& y, double& theta, const sensor_msgs::Image& image);
+    void publishMessage(double& x, double& y, double& theta, const sensor_msgs::Image &image);
+    //getDelta-Service:
+    bool getDelta(shutter::delta::Request &req, shutter::delta::Response &res); 
     double checkPicture(double x, double y, double theta);
+
 
     ros::Subscriber pose_sub;
     ros::Subscriber image_sub;
     ros::Publisher poseImage_pub;
-    mobots_msgs::ImagePoseID ipid;
+    mobots_msgs::ImageWithDeltaPoseAndID ipid;
     
     
 private:
     int id;
     Geometry g;
-    double overlap;
-    double dX, dY, dTheta, globalX, globalY, globalTheta;
+    double overlap, dX, dY, dTheta;
     int argc;
-    char argv;
+    char argv;	
     ros::NodeHandle nh;
 
 };
