4f5430447afd279b57e1a37b681c439c091805ee
Moritz Ulmer <moritz.ulmer@posteo.de>
2012-08-20 19:36:23 +0530

    Image Store works. Manual for the usage and API can be found in the wiki. To test:
    rosrun map_visualization image_store
    rosrun map_visualizaiion image_store_test "/home/moritz/SampleImage.jpg"

--- map_visualization/src/image_store/image_store.cpp
+++ map_visualization/src/image_store/image_store.cpp
@@ -16,40 +16,14 @@ char naming[] = "%ssession-%i/mobotID%i-%i.%s"; // /home/john/session-1/mobotID2
 int *imageCounter;
 
 /**
- * Create a directory.
- * @Param path Takes the full filepath. Removes the filename and recursively
- * creates the directory tree. 
- */
-void mkdir(char* path){
-	size_t pos = 0;
-	int pathLength = 0;
-	while(path[pathLength] != '\0'){
-		if(path[pathLength] == '/'){
-			pos = pathLength;
-		}
-		pathLength++;
-	}
-	char subPath[pos + 2];
-	
-	for(uint i = 0; i < pos + 1; i++){
-		subPath[i] = path[i];
-	}
-	subPath[pos + 1] = '\0';
-	std::string s = subPath;
-	boost::filesystem::create_directories(s);
-}
-
-/**
  * The imageHandler method. 
  * Image naming convention: mobotID-imageNo.jpeg
  * TODO add session IDs and folders
  * TODO check if a session is already has images
  */
 void imageHandlerIn(const mobots_msgs::ImageWithDeltaPoseAndID::ConstPtr& msg){
-	float a = 0;
-	ImageInfo info(0, msg->mobot_id, imageCounter[msg->mobot_id], msg->pose.x,  msg->pose.y, msg->pose.theta, "jpg", msg->image.data);
-	//ImageInfo info(0, msg->mobot_id, imageCounter[msg->mobot_id], msg->pose.x, msg->pose.y, msg->pose.theta, "jpg", msg->image.data);	
-	
+	ImageInfo info(0, msg->mobot_id, imageCounter[msg->mobot_id], msg->pose.x,  msg->pose.y, msg->pose.theta, msg->image.encoding.c_str(), msg->image.data);
+	ROS_INFO("image_store: image saved: %i", imageCounter[msg->mobot_id]);
 	imageCounter[msg->mobot_id]++;
 }
 
@@ -88,8 +62,8 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "image_store_server");
 	ros::NodeHandle n;
 	// The topic name is mobot_image_pose
-	ros::Subscriber sub = n.subscribe("image_in", 10, imageHandlerIn);
-	ros::ServiceServer service = n.advertiseService("image_out", imageHandlerOut);
+	ros::Subscriber sub = n.subscribe("image_store_save", 10, imageHandlerIn);
+	ros::ServiceServer service = n.advertiseService("image_store_get", imageHandlerOut);
 	
 	imageCounter = new int[mobotNumber];
 	for(int i = 0; i < mobotNumber; i++){
