c4a7ac357e8b96b3c4b23c4ce5345e3ef4f1bdc7
Moritz Ulmer <moritz89@gmail.com>
2012-08-28 17:41:47 +0530

    Code Cleanups, errorStatus table

---
+++ map_visualization/src/image_store/errorStatus.cpp
@@ -0,0 +1,4 @@
+100 = Read Error
+101 = Read Error: Bad lexical cast
+102 = Read Error: Info file not found
+103 = Read Error: Image file not found
--- map_visualization/src/image_store/image_store.cpp
+++ map_visualization/src/image_store/image_store.cpp
@@ -21,45 +21,13 @@ char naming[] = "%ssession-%i/mobotID%i-%i.%s"; // /home/john/session-1/mobotID2
 int *imageCounter;
 
 /**
-<<<<<<< HEAD
  * This Method saves incoming messages. The logic is found in
  * "image_info". The imageCounter is incremented.
-=======
- * Create a directory.
- * @Param path Takes the full filepath. Removes the filename and recursively
- * creates the directory tree. 
- */
-void mkdir(char* path){
-	size_t pos = 0;
-	int pathLength = 0;
-	while(path[pathLength] != '\0'){
-		if(path[pathLength] == '/'){
-			pos = pathLength;
-		}
-		pathLength++;
-	}
-	char subPath[pos + 2];
-	
-	for(uint i = 0; i < pos + 1; i++){
-		subPath[i] = path[i];
-	}
-	subPath[pos + 1] = '\0';
-	std::string s = subPath;
-	boost::filesystem::create_directories(s);
-}
-
-/**
- * The imageHandler method. 
- * Image naming convention: mobotID-imageNo.jpeg
- * TODO add session IDs and folders
->>>>>>> 7600c0cfd7825244935da494abc68b77bcb95055
  * TODO check if a session is already has images
  */
 void imageHandlerIn(const mobots_msgs::ImageWithDeltaPoseAndID::ConstPtr& msg){
-	float a = 0;
-	ImageInfo info(0, msg->mobot_id, imageCounter[msg->mobot_id], msg->pose.x,  msg->pose.y, msg->pose.theta, "jpg", msg->image.data);
-	//ImageInfo info(0, msg->mobot_id, imageCounter[msg->mobot_id], msg->pose.x, msg->pose.y, msg->pose.theta, "jpg", msg->image.data);	
-	
+	ImageInfo info(0, msg->mobot_id, imageCounter[msg->mobot_id], msg->pose.x,  msg->pose.y, msg->pose.theta, msg->image.encoding.c_str(), msg->image.data);
+	ROS_INFO("image_store: image saved: %i", imageCounter[msg->mobot_id]);
 	imageCounter[msg->mobot_id]++;
 }
 
@@ -75,24 +43,6 @@ bool imageHandlerOut(map_visualization::GetImage::Request &req, map_visualizatio
 	}
 	
 	res.image.image.data = info.getImageData();
-	/*std::ifstream::pos_type size;
-	std::ifstream imageFile(info.getImagePath(), std::ios::binary);
-	if(imageFile.is_open()){
-		imageFile.seekg (0, std::ios::end);
-		size = imageFile.tellg();
-		char* fileData = new char[size];
-		imageFile.seekg (0, std::ios::beg);
-		imageFile.read (fileData, size); // Image Data
-		imageFile.close();
-		
-  		const std::vector<unsigned char> fifth (fileData, fileData + sizeof(fileData) / sizeof(int) );
-		res.image.image.data = fifth;
-		delete fileData;
-	} else {
-	// Error 1 = Image not found
-		res.error = 1;
-		return true;
-	}*/
 	return true;
 }
 
@@ -106,16 +56,10 @@ int main(int argc, char **argv){
 	// The node is called image_store_server
 	ros::init(argc, argv, "image_store_server");
 	ros::NodeHandle n;
-<<<<<<< HEAD
 	// To save images: image_store_save
 	// To get images: image_store_get
 	ros::Subscriber sub = n.subscribe("image_store_save", 10, imageHandlerIn);
 	ros::ServiceServer service = n.advertiseService("image_store_get", imageHandlerOut);
-=======
-	// The topic name is mobot_image_pose
-	ros::Subscriber sub = n.subscribe("image_in", 10, imageHandlerIn);
-	ros::ServiceServer service = n.advertiseService("image_out", imageHandlerOut);
->>>>>>> 7600c0cfd7825244935da494abc68b77bcb95055
 	
 	imageCounter = new int[mobotNumber];
 	for(int i = 0; i < mobotNumber; i++){
