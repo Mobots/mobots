5925e6159d7b55c2f987598aab7a9490dc87fe9b
Moritz Ulmer <moritz89@gmail.com>
2012-09-10 03:49:45 +0200

    Proper loading, deleting of imageMpaVisual objects in Rviz

--- map_visualization/src/image_map_display/image_map_test.cpp
+++ map_visualization/src/image_map_display/image_map_test.cpp
@@ -11,9 +11,9 @@
 #include "vector"
 
 /**
- * This is an example of how to interface with the image_store server.
+ * "image_store_rel_pose" (ImageWithPoseAndID) test publisher
  */
-void saveRequest(std::string filePath, ros::NodeHandle* handle){
+void relPublisher(std::string filePath, ros::NodeHandle* handle){
 	ros::Publisher pub = handle->advertise<mobots_msgs::ImageWithPoseAndID>("image_store_save", 10);
 	mobots_msgs::ImageWithPoseAndID msg;
 	// Set image info data
@@ -21,7 +21,9 @@ void saveRequest(std::string filePath, ros::NodeHandle* handle){
 	msg.pose.y = 42.23;
 	msg.pose.theta = 30.00;
 	msg.image.encoding = "jpg";
+	msg.id.session_id = 1;
 	msg.id.mobot_id = 3;
+	msg.id.image_id = 4;
 	// Load image data
 	std::ifstream imageFile(filePath.c_str(), std::ios::binary);
 	imageFile.seekg(0, std::ios::end);
@@ -33,7 +35,7 @@ void saveRequest(std::string filePath, ros::NodeHandle* handle){
 	msg.image.data.assign(buffer, buffer + sizeof(buffer)/sizeof(char));
 	ROS_INFO("data.size: %i", msg.image.data.size());
 	
-	ros::Rate loop_rate(1);
+	ros::Rate loop_rate(0.3);
 	
 	int count = 0;
 	while (handle->ok())
@@ -42,36 +44,47 @@ void saveRequest(std::string filePath, ros::NodeHandle* handle){
 		pub.publish(msg);
 		ros::spinOnce();
 		loop_rate.sleep();
-		// Change image info data (pose)
 		msg.pose.x++;
 		msg.pose.y++;
+		msg.id.image_id++;
 		count++;
 	}
 	return;
 }
 
-void getRequest(int sessionID, int mobotID, int imageID, ros::NodeHandle* handle){
-	ros::ServiceClient client = handle->serviceClient<map_visualization::GetImageWithPose>("image_store_get");
-	map_visualization::GetImageWithPose srv;
-	srv.request.id.session_id = sessionID;
-	srv.request.id.mobot_id = mobotID;
-	srv.request.id.image_id = imageID;
+void absPublisher(int sessionID, int mobotID, int imageID, ros::NodeHandle* handle){
+	ros::Publisher pub = handle->advertise<mobots_msgs::ImageWithPoseAndID>("image_store_abs_pose", 10);
+	mobots_msgs::ImageWithPoseAndID msg;
+	// Set image info data
+	msg.pose.x = 0;
+	msg.pose.y = 0;
+	msg.pose.theta = 0;
+	msg.image.encoding = "jpg";
+	msg.id.session_id = sessionID;
+	msg.id.mobot_id = mobotID;
+	msg.id.image_id = imageID;
+	// Load image data
 	
-	if(client.call(srv)){
-		if(srv.response.error == 0){
-			ROS_INFO("Returned image");
-		} else {
-			ROS_INFO("Error Message: %i", srv.response.error);
-		}
-	} else {
-		ROS_INFO("Errror Message: Server Error");
+	ros::Rate loop_rate(0.5);
+	
+	int count = 0;
+	while (handle->ok())
+	{
+		ROS_INFO("Sent msg no: %i", count);
+		pub.publish(msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+		msg.pose.x--;
+		msg.pose.y++;
+		msg.pose.theta++;
+		msg.id.image_id++;
+		count++;
 	}
 	return;
 }
 
 /**
- * This server is used to check if the toro_client.cpp works.
- * The ImagePoseID msg is tested.
+ * This server checks if image_map_display.cpp works.
  */
 int main(int argc, char **argv)
 {
@@ -83,12 +96,12 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "image_store_test");
 	ros::NodeHandle handle;
 	
-	if(argument1 == "save" && argc == 3){
-		saveRequest(std::string(argv[2]), &handle);
-	} else if(argument1 == "get" && argc == 5){
-		getRequest(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &handle);
+	if(argument1 == "rel" && argc == 3){
+		relPublisher(std::string(argv[2]), &handle);
+	} else if(argument1 == "abs" && argc == 5){
+		absPublisher(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), &handle);
 	} else {
-		ROS_INFO("Usage: image_store_test [save /path/to/file.jpg] [get sessionID mobotID imageID]");
+		ROS_INFO("Usage: image_store_test [rel /path/to/file.jpg] [abs]");
 	}
 	return 0;
 }
