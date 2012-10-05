ab36e345b0ae23bc2e9390ca6c5de3760badb33a
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-10-05 20:02:45 +0200

    poll image from cam

--- shutter/src/manualShutter.cpp
+++ shutter/src/manualShutter.cpp
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <iostream>
 #include <ros/ros.h>
+#include <sensor_msgs/fill_image.h>
+#include <usb_cam/usb_cam.h>
 
 #include "mobots_msgs/ImageWithPoseAndID.h"
 
@@ -9,6 +11,7 @@ using namespace std;
 const char TAG[] = "[manualShutter] ";
 sensor_msgs::Image currentImage;
 ros::Publisher pub;
+usb_cam_camera_image_t* camera_image_;
 
 void imageCallback(const sensor_msgs::Image &image){
   currentImage = image;
@@ -21,9 +24,12 @@ void* shutterThread(void* data){
   int sessionID = 0;
   while(ros::ok()){
     cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
-    msg.image = currentImage;
-    pub.publish(msg);
-	msg.id.image_id++;
+	 for(int i = 0; i < 7; i++)
+		usb_cam_camera_grab_image(camera_image_);
+    fillImage(msg.image, "rgb8", camera_image_->height, camera_image_->width, 3 * camera_image_->width, camera_image_->image);
+    //msg.image = currentImage;
+	 pub.publish(msg);
+	 msg.id.image_id++;
     cout << TAG << "shuttered" << endl;
   }
 }
@@ -31,10 +37,16 @@ void* shutterThread(void* data){
 int main(int argc, char **argv){
   ros::init(argc, argv, "manualShutter");
   ros::NodeHandle nh;
-  pub = nh.advertise<mobots_msgs::ImageWithPoseAndID>("shutter_image_delta_pose", 2);
-  ros::Subscriber image_sub = nh.subscribe("usb_cam/image_raw", 1, imageCallback);
+  pub = nh.advertise<mobots_msgs::ImageWithPoseAndID>("/mobot1/ImageWithPoseAndID", 2);
+  //ros::Subscriber image_sub = nh.subscribe("usb_cam/image_raw", 1, imageCallback);
   pthread_t thread_t;
   pthread_create(&thread_t, 0, shutterThread, 0);
   cout << TAG << "Press [enter] for shutter" << endl;
+  camera_image_ = usb_cam_camera_start("/dev/video0",
+        IO_METHOD_MMAP,
+        PIXEL_FORMAT_YUYV,
+        640,
+        480);
   ros::spin();
+  usb_cam_camera_shutdown();
 }
