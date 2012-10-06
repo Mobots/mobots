359a7702fe447d7dd92851be46d0013403fdd79d
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-10-06 19:13:07 +0200

    cleanup, preperations for infrared

--- hardware_driver/src/hardware_driver.cpp
+++ hardware_driver/src/hardware_driver.cpp
@@ -1,27 +1,83 @@
-#include <ros/ros.h>
+#include "ros/ros.h"
 #include <iostream>
 #include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
 
 #include "geometry_msgs/Pose2D.h"
 
-//#define USE_DUAL
-
 using namespace std;
 
-const char TAG[] = "[(simul) hardware_driver] ";
+const char TAG[] = "[hardware_driver] ";
 
-int frequency = 30; //Hz
+// -- values in Hz --
+int mouseFrequency = 10;
+int infraredFrequency = 10;
+
 ros::NodeHandle *nh;
-int mouse1, mouse2;
+int mouse1FD, mouse2FD, infraredFD;
 
-void* singleController(void *data){
-  ros::Rate rate(frequency);
+void* singleMouseReader(void*);
+void* dualMouseReader(void*);
+void* infraredReader(void*);
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "hardware_driver");
+  nh = new ros::NodeHandle("~");
+  string mousePath;
+  
+  nh->param("path1", mousePath, string("/dev/input/mouse1"));
+  mouse1FD = open(mousePath.c_str(), O_RDONLY);
+  if(mouse1FD < 0){
+    cerr << TAG << "cannot open " << mousePath << endl;
+	 ROS_ERROR("%s cannot open %s", TAG, mousePath.c_str());
+    exit(1);
+  }
+  
+  int miceCount;
+  nh->param("miceCount", miceCount, 1);
+  if(miceCount > 1){
+    nh->param("path2", mousePath, string("/dev/input/mouse2"));
+    int mouse2 = open(mousePath.c_str(), O_RDONLY);
+    if(mouse2 < 0){
+		ROS_ERROR("%s cannot open %s", TAG, mousePath.c_str());
+      cerr << TAG << "cannot open " << mousePath << endl;
+      exit(1);
+    }
+  }
+  
+  /*string infraredPath;
+  nh->param("irpath", infraredPath, string("/dev/input/infrared"));
+  infraredFD = open(infraredPath.c_str(), O_RDONLY);
+  if(infraredFD < 0){
+	 cerr << TAG << "cannot open " << infraredPath << endl;
+	 ROS_ERROR("%s cannot open %s", TAG, infraredPath.c_str());
+    exit(1);
+  }*/
+  
+  string mouseFrequencyKey("/hardware_driver/mouseFrequency");
+  string infraredFrequencyKey("/hardware_driver/infraredFrequency");
+  ros::param::get(mouseFrequencyKey, mouseFrequency);
+  ros::param::get(infraredFrequencyKey, infraredFrequency);
+  
+  pthread_t thread_t;
+  if(miceCount > 1)
+    pthread_create(&thread_t, 0, dualMouseReader, 0);
+  else
+    pthread_create(&thread_t, 0, singleMouseReader, 0);
+    
+  pthread_create(&thread_t, 0, infraredReader, 0);
+  
+  ros::spin();
+}
+
+void* singleMouseReader(void* data){
+  ros::Rate rate(mouseFrequency);
   ros::Publisher pub = nh->advertise<geometry_msgs::Pose2D>("mouse/pose", 2);
   geometry_msgs::Pose2D deltaPose;
   char data1[3];
   while(ros::ok()){
-    read(mouse1, &data1, 3);
+    read(mouse1FD, &data1, 3);
     deltaPose.x = data1[1];
     deltaPose.y = data1[2];
     deltaPose.theta = 0;
@@ -30,19 +86,19 @@ void* singleController(void *data){
   }
 }
 
-void* dualController(void *data){
+void* dualMouseReader(void* data){
   char data1[3];
   char data2[3];
-  ros::Rate rate(frequency);
+  ros::Rate rate(mouseFrequency);
   ros::Publisher pub = nh->advertise<geometry_msgs::Pose2D>("mouse/pose", 2);
   geometry_msgs::Pose2D deltaPose;
   while(ros::ok()){
     char xAvg = 0;
     char yAvg = 0;
     char theta = 0;
-    read(mouse1, &data1, 3);
-    read(mouse2, &data2, 3);
-    xAvg = (data1[1]/2) + (data2[1]/2) + ((data1[1]&0x1) & (data2[1]&0x1));
+    read(mouse1FD, &data1, 3);
+    read(mouse2FD, &data2, 3);
+    xAvg = (data1[1]/2) + (data2[1]/2) + ((data1[1]&0x1) & (data2[1]&0x1)); // zweiter Teil um Rundungsfehler zu vermeiden
     yAvg = (data1[2]/2) + (data2[2]/2) + ((data1[2]&0x1) & (data2[2]&0x1));
     //das folgende muss noch durch nen induktionsbeweis verifiziert werden :D
     theta = atan2(data2[2]-data1[2], data2[1]-data1[1]);
@@ -55,35 +111,12 @@ void* dualController(void *data){
   }
 }
 
-int main(int argc, char **argv){
-  ros::init(argc, argv, "hardware_driver");
-  nh = new ros::NodeHandle("~");
-  string mousePath;
-  
-  nh->param("path1", mousePath, string("/dev/input/mouse1"));
-  mouse1 = open(mousePath.c_str(), O_RDONLY);
-  if(mouse1 < 0){
-    cerr << TAG << "cannot open " << mousePath << endl;
-    exit(1);
-  }
-  
-  int miceCount;
-  nh->param("miceCount", miceCount, 1);
-  if(miceCount > 1){
-    nh->param("path2", mousePath, string("/dev/input/mouse2"));
-    int mouse2 = open(mousePath.c_str(), O_RDONLY);
-    if(mouse2 < 0){
-      cerr << TAG << "cannot open mouse 2" << endl;
-      exit(1);
-    }
-  }
-  
-  pthread_t thread_t;
-  if(miceCount > 1)
-    pthread_create(&thread_t, 0, dualController, 0);
-  else
-    pthread_create(&thread_t, 0, singleController, 0);
-    
-  ros::spin();
-    
+void* infraredReader(void* data){
+  /*ros::Rate rate(infraredFrequency);
+  ros::Publisher pub = nh->advertise<>("", 2);
+  while(ros::ok()){
+    read(infraredFD, XX, X);
+    pub.publish(XX);
+    rate.sleep();
+  }*/
 }
