6b616dfb8762d393940ddaffeb56ea2c9d587013
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-10-15 23:56:15 +0200

    Patrick: let's take all the declarations from the source files and put them into header files

--- image_store/src/feature.cpp
+++ image_store/src/feature.cpp
@@ -1,28 +1,28 @@
 #include <fstream>
-
-#include "mobots_msgs/FeatureSetWithPoseAndID.h"
-#include "image_pose_data_types.h"
 #include <sys/stat.h>
+
+#include "feature.h"
+#include "image_pose_data_types.h"
 
 namespace ser = ros::serialization;
 
-const char* HOME = getenv("HOME");
+static const char* HOME = getenv("HOME");
 
-std::string getPathForIDT(const IDT id, const std::string& fileEnding){
+static std::string getPathForIDT(const IDT id, const char* fileEnding){
   std::stringstream ss;
   ss << HOME << "/session-" << id.sessionID << "/mobot-" << id.mobotID << "-" << id.imageID << fileEnding;
   return ss.str();
 }
 
-namespace FeatureStore{
-
-bool loadFeatureSet(const IDT idt, mobots_msgs::FeatureSetWithPoseAndID& featureSet){
-  uint32_t serial_size = ros::serialization::serializationLength(featureSet);
-	std::string path = getPathForIDT(idt, "");
-	struct stat filestatus;
+bool FeatureStore::loadFeatureSet(const IDT& idt, mobots_msgs::FeatureSetWithPoseAndID& featureSet){
+  std::string path = getPathForIDT(idt, "");
+  struct stat filestatus;
   stat(path.c_str(), &filestatus);
-	long size = filestatus.st_size;
-  std::ifstream in(getPathForIDT(idt, "").c_str(), std::ios::binary);
+  long size = filestatus.st_size;
+  std::ifstream in;
+  in.open(getPathForIDT(idt, "").c_str(), std::ios::binary);
+  if(!in)
+	 return false;
   unsigned char* data = new unsigned char[size];
   in.read((char*) data, size);
   ser::IStream stream(data, size);
@@ -32,8 +32,8 @@ bool loadFeatureSet(const IDT idt, mobots_msgs::FeatureSetWithPoseAndID& feature
   return true;
 }
 
-bool saveFeatureSet(const mobots_msgs::FeatureSetWithPoseAndID& featureSet){
-  uint32_t serial_size = ros::serialization::serializationLength(featureSet);
+bool FeatureStore::saveFeatureSet(const mobots_msgs::FeatureSetWithPoseAndID& featureSet){
+  uint32_t serial_size = ser::serializationLength(featureSet);
   unsigned char* data = new unsigned char[serial_size];
   ser::OStream stream(data, serial_size);
   ser::serialize(stream, featureSet);
@@ -43,11 +43,11 @@ bool saveFeatureSet(const mobots_msgs::FeatureSetWithPoseAndID& featureSet){
   idt.imageID = featureSet.id.image_id;
   std::ofstream out;
   out.open(getPathForIDT(idt, "").c_str(), std::ios::binary);
+  if(!out)
+	 return false;
   out.write((const char*) data, serial_size);
   out.flush();
   out.close();
   delete[] data;
   return true;
 }
-
-};
--- image_store/src/image_pose.cpp
+++ image_store/src/image_pose.cpp
@@ -8,65 +8,16 @@
 #include <boost/filesystem.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
+#include <ros/console.h>
 
-#include "image_pose_data_types.cpp"
+#include "image_pose_data_types.h"
+#include "image_pose.h"
 
 // Filename ending for the files
 std::string savePathRoot("~/"); // save path root
 std::string fileConvention("mobotID%i-%i.%s"); // mobotID2-12.jpg
 std::string folderConvention("%ssession-%i/"); // /home/john/session-1/
 std::string infoEnding("info");
-
-/**
- * Saves an image and its infos in a file or loads the image and infos.
- * TODO calculate imagePath size
- */
-class ImagePose{
-public:
-	ImagePose();
-	ImagePose(const IDT* id);
-	ImagePose(const imagePoseData* infoData_);
-	ImagePose(const imagePoseData* infoData_, const std::vector<unsigned char>);
-
-	int getErrorStatus(){return errorStatus;}
-	
-	poseT getDelPose(){return infoData.delPose;}
-	void setDelPose(const poseT* pose){infoData.delPose = *pose;}
-	
-	poseT getRelPose(){return infoData.relPose;}
-	void setRelPose(const poseT* pose){infoData.relPose = *pose;}
-	
-	poseT getAbsPose(){return infoData.absPose;}
-	void setAbsPose(const poseT* pose){infoData.absPose = *pose;}
-	
-	IDT getID(){return infoData.id;}
-	void setID(const IDT* id){infoData.id = *id;}
-	
-	std::string getEncoding(){return infoData.image.encoding;}
-	void setEncoding(const std::string* encoding)
-		{infoData.image.encoding = *encoding;}
-	int getWidth(){return infoData.image.width;}
-	void setWidth(int width){infoData.image.width = width;}
-	int getHeight(){return infoData.image.height;}
-	void setHeight(int height){infoData.image.height = height;}
-	
-	std::vector<unsigned char> getImageData();
-	void loadLast(const IDT* id_);
-private:
-	std::string savePath;
-	int errorStatus;
-	
-	std::string infoPath;
-	imagePoseData infoData;
-	
-	std::string imagePath;
-	std::vector<unsigned char> imageData;
-	
-	char* concPath(const char*);
-	char* concPath();
-	int initReadImage();
-	int initWrite();
-};
 
 ImagePose::ImagePose(){
 	savePath = savePathRoot;
