ddbba5f0f1fde93f630e0fc4b16c247a07636f88
Moritz Ulmer <moritz.ulmer@posteo.de>
2012-10-22 18:50:59 +0200

    Initial image_map_info in map_visualization

--- map_visualization_helper/src/image_map_info.cpp
+++ map_visualization_helper/src/image_map_info.cpp
@@ -1,19 +1,33 @@
 #include "image_map_info.h"
 
-namespace map_visualization_helper{
+int main(int argc, char *argv[]){
+    QApplication a(argc, argv);
+    map_visualization::ImageMapModel imageMapModel(0);
 
-ImageMapInfo::ImageMapInfo(QWidget *parent) :
-    QMainWindow(parent), ui(new Ui::ImageMapInfo)
-{
-    ROS_INFO("[ImageMapInfo]");
-    ui->setupUi(this);
-    ImageMapModel imageMapModel(0);
-    ui->mobotInfoTable->setModel(&imageMapModel);
-    ROS_INFO("[ImageMapInfo]");
-}
+    QHBoxLayout* headerBox = new QHBoxLayout();
+    QLabel* sessionLabel = new QLabel("Session");
+    QComboBox* sessionComboBox = new QComboBox(0);
+    sessionComboBox->setModel(&imageMapModel);
+    headerBox->addWidget(sessionLabel);
+    headerBox->addWidget(sessionComboBox);
+    headerBox->addStretch(0);
+    QLabel* waypointLabel = new QLabel("waypoint for Mobot");
+    QComboBox* waypointComboBox = new QComboBox(0);
+    waypointComboBox->setModel(&imageMapModel);
+    headerBox->addWidget(waypointLabel);
+    headerBox->addWidget(waypointComboBox);
+    headerBox->addStretch(0);
 
-ImageMapInfo::~ImageMapInfo(){
-    delete ui;
-}
+    QVBoxLayout* centralWidget = new QVBoxLayout();
+    QTableView* infoTable = new QTableView();
+    infoTable->setModel(&imageMapModel);
+    centralWidget->addLayout(headerBox);
+    centralWidget->addWidget(infoTable);
 
+    QWidget* window = new QWidget(0);
+    window->setLayout(centralWidget);
+    window->setWindowTitle("Image Map Info");
+    window->show();
+
+    return a.exec();
 }
--- map_visualization_helper/src/image_map_info.h
+++ map_visualization_helper/src/image_map_info.h
@@ -1,28 +1,7 @@
 #ifndef IMAGE_MAP_INFO_H
 #define IMAGE_MAP_INFO_H
 
-#include <QMainWindow>
-
-#include <ros/ros.h>
-
-#include "ui_image_map_info.h"
+#include <QtGui>
 #include "image_map_model.h"
 
-namespace Ui {
-    class ImageMapInfo;
-}
-
-namespace map_visualization_helper{
-
-class ImageMapInfo : public QMainWindow
-{
-    Q_OBJECT
-public:
-    explicit ImageMapInfo(QWidget *parent = 0);
-    ~ImageMapInfo();
-private:
-    Ui::ImageMapInfo *ui;
-};
-
-}
 #endif // IMAGE_MAP_INFO_H
