8ba066755a29221ec6989196585621bcad3ab70d
Moritz Ulmer <moritz.ulmer@posteo.de>
2012-10-25 20:02:02 +0200

    waypoint added to map_visualization

--- map_visualization_helper/src/image_map_waypoint.cpp
+++ map_visualization_helper/src/image_map_waypoint.cpp
@@ -2,13 +2,17 @@
 
 namespace map_visualization{
 
-ImageMapWaypoint::ImageMapWaypoint(QObject *parent) :
-    QObject(parent)
-{
-    int argc = 0;
-    char* argv[];
-    argv = &"image_map_info";
+void ImageMapWaypoint::run(){
+    qDebug() << "hello from worker thread " << thread()->currentThreadId();
+    int argc = 1;
+    char* argv[4];
+    *argv = new char[4];
+    argv[0][0] = 'a';
+    argv[0][0] = 'b';
+    argv[0][0] = 'c';
+    argv[0][0] = '\0';
     ros::init(argc, argv, "image_map_info");
+    ROS_INFO("I'm alive");
 }
 
 }
