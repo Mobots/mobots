dae3ed352977f212d125ad0e3445d87d26b8ed0d
Mobots Server Account <grp-mobots@informatik.uni-bremen.de>
2012-11-08 20:35:17 +0100

    obamaown

--- slam/src/slam.h
+++ slam/src/slam.h
@@ -24,11 +24,21 @@ private:
 /* Constants */
     static const uint ITERATIONS_PER_NEW_IMAGE = 7;
     enum EdgeState {MATCHING_IMPOSSIBLE = 0, MATCHED};
-    static const uint32_t NOT_EXISTENT_YET = 0xffffffff;
-
+    static const uint32_t NOT_RECEIVED_YET = 0xffffffff;
+    
+/*    static const float matching_varianz_translation;
+    static const float matching_varianz_rotation;
+    static const AISNavigation::TreeOptimizer2::InformationMatrix matching_covarianz_matrix;
+    
+    static const float mouse_varianz_translation;
+    static const float mouse_varianz_rotation;
+    static const AISNavigation::TreeOptimizer2::InformationMatrix mouse_covarianz_matrix;
+*/
+    
 /* Member variables */
     uint32_t last_id_[mobots_common::constants::mobot_count];
     uint32_t current_id_[mobots_common::constants::mobot_count];
+    AISNavigation::TreeOptimizer2::Transformation start_pose_[mobots_common::constants::mobot_count];
 
     ros::NodeHandle node_handle_;
     ros::Subscriber subscriber_[mobots_common::constants::mobot_count];
@@ -39,19 +49,26 @@ private:
     std::map<uint32_t, FeatureSet> feature_sets_;
     CpuFeaturesMatcher features_matcher_;
     
-    std::map< std::pair<uint32_t,uint32_t> , EdgeState > edge_states_; 
+    typedef std::map< std::pair<uint32_t,uint32_t> , EdgeState > EdgeStateMap;
+    EdgeStateMap edge_states_; 
 
 /* Member functions */
     void callback(const boost::shared_ptr<mobots_msgs::FeatureSetWithPoseAndID const>& msg, uint mobot_id);
     void addNewVertexToGraph(const boost::shared_ptr<mobots_msgs::FeatureSetWithPoseAndID const>& msg, uint bot);
-    void findEdgesBruteforce();
     enum Slam::EdgeState tryToMatch(const uint32_t v, const uint32_t w);
-    void publishOptimizedPoses();
+    void addNewVertexFromMouseData(const boost::shared_ptr<mobots_msgs::FeatureSetWithPoseAndID const>& msg, uint bot);
+    
+    void findEdgesBruteforce();
+    void tryToMatchWithAllOthers(const AISNavigation::TreeOptimizer2::VertexMap::value_type &v, bool distance_check = true);
+
     void runToro();
+    void publishOptimizedPoses();
     
 /* Helper functions */
     uint32_t merge(mobots_msgs::ID const &id);
     mobots_msgs::ID split(uint32_t id);
+    std::string print(uint32_t id);
+    
     AISNavigation::TreeOptimizer2::Transformation convertPixelsToMeters(geometry_msgs::Pose2D pose);
     geometry_msgs::Pose2D convert(AISNavigation::TreeOptimizer2::Pose toro_pose);
 };
