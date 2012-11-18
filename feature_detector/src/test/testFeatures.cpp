fb9e2fea1e6b9d82478a8f6432d4eab2ca62bb40
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-11-18 19:53:06 +0100

    asdfsafalskjlsadkjlsadfjlsdfkj

--- feature_detector/src/test/testFeatures.cpp
+++ feature_detector/src/test/testFeatures.cpp
@@ -204,7 +204,7 @@ int main(int argc, char** argv){
   copyMatToImageMSg(image2Gray, i2);
   ros::NodeHandle nodeHandle;
   ros::Subscriber subscriber = nodeHandle.subscribe("/mobot1/featureset_pose_id", 2, featuresReceived);
-  ros::Publisher publisher = nodeHandle.advertise<mobots_msgs::ImageWithPoseAndID>("/mobot1/image_pose_id", 2);
+  ros::Publisher publisher = nodeHandle.advertise<mobots_msgs::ImageWithPoseAndID>("/mobot1/image_pose_id_relay", 2);
   cout << "now sending" << endl;
   sleep(1);
   publisher.publish(i1);
