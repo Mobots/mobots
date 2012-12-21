8e649eab5412138bfccf7dabc13ec9ca4474c036
Jonas Rahlf <dev.jonas.rahlf@gmail.com>
2012-12-21 14:28:11 +0100

    sdfsadf

--- hardware_driver/src/hardware_driver.cpp
+++ hardware_driver/src/hardware_driver.cpp
@@ -128,12 +128,12 @@ void sensorValHandler(enum PROTOCOL_IDS id, unsigned char *data,
 
 //***************************+++ Transform +++********************************
     //mobot-> global
-  double cost = cos(globalPose.theta);
-  double sint = sin(globalPose.theta);
+  double cost = cos(-globalPose.theta);
+  double sint = sin(-globalPose.theta);
 	double x = delta_vals->x;
 	double y = delta_vals->y;
-  delta_vals->y = sint*x + cost*y;
-  delta_vals->x = cost*x - sint*y;
+  //delta_vals->y = sint*x + cost*y;
+  //delta_vals->x = cost*x - sint*y;
 
 
 
